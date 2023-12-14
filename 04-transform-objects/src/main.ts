3f907ada8cf95ae720a629c3671b936ebfeb4010
Zai Santillan <zaisantillan14@gmail.com>
2023-12-14 12:06:44 +0800

    feat(04): add combining

--- 04-transform-objects/src/main.ts
+++ 04-transform-objects/src/main.ts
@@ -12,6 +12,27 @@ const material = new THREE.MeshBasicMaterial({ color: 'red' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
+
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
 // Sizes
 const sizes = {
   width: 800,
@@ -22,6 +43,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+camera.lookAt(mesh.position)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
