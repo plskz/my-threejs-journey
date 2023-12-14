2f1a828335aed3bfbb5db7a231cf8f83000e3731
Zai Santillan <zaisantillan14@gmail.com>
2023-12-14 12:16:17 +0800

    feat(04): add scene graph

--- 04-transform-objects/src/main.ts
+++ 04-transform-objects/src/main.ts
@@ -7,27 +7,33 @@ const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')!
 const scene = new THREE.Scene()
 
 // Objects
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 'red' })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
-// Scale
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5)
+const cube1 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: 'red' })
+)
+group.add(cube1)
 
-// Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.y = Math.PI * 0.25
-mesh.rotation.x = Math.PI * 0.25
+const cube2 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: 'green' })
+)
+cube2.position.x = -2
+group.add(cube2)
 
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+const cube3 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+cube3.position.x = 2
+group.add(cube3)
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2)
@@ -43,8 +49,6 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
-
-camera.lookAt(mesh.position)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
