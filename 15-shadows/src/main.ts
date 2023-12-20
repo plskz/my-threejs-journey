e2dc734f38facb27db3413e27dfa6975b2c2ba84
Zai Santillan <zaisantillan14@gmail.com>
2023-12-20 23:14:56 +0800

    feat(15): add shadows

--- 15-shadows/src/main.ts
+++ 15-shadows/src/main.ts
@@ -3,8 +3,21 @@ import * as THREE from 'three'
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-import { gsap } from 'gsap'
+// import { gsap } from 'gsap'
 import GUI from 'lil-gui'
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+// bakedShadow.jpg
+// const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
+// bakedShadow.colorSpace = THREE.SRGBColorSpace
+
+// simpleShadow.jpg
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
+simpleShadow.colorSpace = THREE.SRGBColorSpace
 
 /**
  * Base
@@ -27,36 +40,153 @@ const scene = new THREE.Scene()
  */
 // Ambient light
 const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
+const folderAmbientLight = gui.addFolder('Ambient Light')
+folderAmbientLight.add(ambientLight, 'visible')
+folderAmbientLight.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
 directionalLight.position.set(2, 2, -1)
-gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
-gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001)
-gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001)
-gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001)
+const folderDirectionalLight = gui.addFolder('Directional Light')
+folderDirectionalLight.add(directionalLight, 'visible')
+folderDirectionalLight
+  .add(directionalLight, 'intensity')
+  .min(0)
+  .max(3)
+  .step(0.001)
+folderDirectionalLight
+  .add(directionalLight.position, 'x')
+  .min(-5)
+  .max(5)
+  .step(0.001)
+folderDirectionalLight
+  .add(directionalLight.position, 'y')
+  .min(-5)
+  .max(5)
+  .step(0.001)
+folderDirectionalLight
+  .add(directionalLight.position, 'z')
+  .min(-5)
+  .max(5)
+  .step(0.001)
 scene.add(directionalLight)
+
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.setScalar(1024)
+// amplitude
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.camera.left = -2
+// near and far
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 6
+
+// won't work if the renderer.shadowMap.type is set to THREE.PCFSoftShadowMap
+// directionalLight.shadow.radius = 10
+
+const directionalLightCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+)
+directionalLightCameraHelper.visible = false
+folderDirectionalLight
+  .add(directionalLightCameraHelper, 'visible')
+  .name('directionalLightHelper')
+scene.add(directionalLightCameraHelper)
+
+// Spot light
+const spotLight = new THREE.SpotLight(0xffffff, 3.6, 10, Math.PI * 0.3)
+spotLight.castShadow = true
+spotLight.shadow.mapSize.setScalar(1024)
+spotLight.shadow.camera.fov = 30
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
+
+spotLight.position.set(0, 2, 2)
+scene.add(spotLight, spotLight.target)
+
+const folderSpotLight = gui.addFolder('Spot Light')
+folderSpotLight.add(spotLight, 'visible')
+folderSpotLight.add(spotLight, 'intensity').min(0).max(3).step(0.001)
+folderSpotLight.add(spotLight.position, 'x').min(-5).max(5).step(0.001)
+folderSpotLight.add(spotLight.position, 'y').min(-5).max(5).step(0.001)
+folderSpotLight.add(spotLight.position, 'z').min(-5).max(5).step(0.001)
+folderSpotLight
+  .add(spotLight, 'angle')
+  .min(0)
+  .max(Math.PI / 2)
+  .step(0.001)
+
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+spotLightCameraHelper.visible = false
+scene.add(spotLightCameraHelper)
+folderSpotLight.add(spotLightCameraHelper, 'visible').name('spotLightHelper')
+
+// Point light
+const pointLight = new THREE.PointLight(0xffffff, 2.7)
+pointLight.castShadow = true
+pointLight.shadow.mapSize.setScalar(1024)
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 5
+
+pointLight.position.set(-1, 1, 0)
+scene.add(pointLight)
+
+const folderPointLight = gui.addFolder('Point Light')
+folderPointLight.add(pointLight, 'visible')
+folderPointLight.add(pointLight, 'intensity').min(0).max(3).step(0.001)
+folderPointLight.add(pointLight.position, 'x').min(-5).max(5).step(0.001)
+folderPointLight.add(pointLight.position, 'y').min(-5).max(5).step(0.001)
+folderPointLight.add(pointLight.position, 'z').min(-5).max(5).step(0.001)
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+pointLightCameraHelper.visible = false
+scene.add(pointLightCameraHelper)
+folderPointLight.add(pointLightCameraHelper, 'visible').name('pointLightHelper')
 
 /**
  * Materials
  */
 const material = new THREE.MeshStandardMaterial()
 material.roughness = 0.7
-gui.add(material, 'metalness').min(0).max(1).step(0.001)
-gui.add(material, 'roughness').min(0).max(1).step(0.001)
+const folderMaterial = gui.addFolder('Material')
+folderMaterial.add(material, 'metalness').min(0).max(1).step(0.001)
+folderMaterial.add(material, 'roughness').min(0).max(1).step(0.001)
 
 /**
  * Objects
  */
+// sphere geometry
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material)
+const folderSphere = gui.addFolder('Sphere')
+folderSphere.add(sphere.position, 'x').min(-5).max(5).step(0.001)
+folderSphere.add(sphere.position, 'y').min(-5).max(5).step(0.001)
+folderSphere.add(sphere.position, 'z').min(-5).max(5).step(0.001)
 
+sphere.castShadow = true
+
+// plane geometry
+// new THREE.MeshBasicMaterial({ map: bakedShadow })
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material)
 plane.rotation.x = -Math.PI * 0.5
 plane.position.y = -0.5
+plane.receiveShadow = true
 
-scene.add(sphere, plane)
+// sphere geometry (used for the shadow)
+const sphereShadow = new THREE.Mesh(
+  new THREE.PlaneGeometry(1.5, 1.5),
+  new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    transparent: true,
+    alphaMap: simpleShadow,
+  })
+)
+
+sphereShadow.rotation.x = -Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphere, plane, sphereShadow)
 
 /**
  * Sizes
@@ -90,7 +220,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 )
-camera.position.set(1, 1, 2)
+camera.position.set(4, 3.7, 5)
 scene.add(camera)
 
 // Controls
@@ -104,6 +234,9 @@ const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+renderer.shadowMap.enabled = false // note: set this to true to enable shadow
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
 /**
  * Animate
  */
@@ -111,6 +244,19 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Update sphere
+
+  // orbit
+  sphere.position.x = Math.cos(elapsedTime) * 1.5
+  sphere.position.z = Math.sin(elapsedTime) * 1.5
+
+  sphere.position.y = Math.abs(Math.sin(elapsedTime * 3)) // bounce effect
+
+  // Update shadow
+  sphereShadow.position.x = sphere.position.x
+  sphereShadow.position.z = sphere.position.z
+  sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3
 
   // Update controls
   controls.update()
