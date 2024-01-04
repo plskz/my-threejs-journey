23173a0f1e2481958f9b3b6a41224523a068ad68
Zai Santillan <zaisantillan14@gmail.com>
2024-01-04 15:06:43 +0800

    perf(20): optimize

--- 20-physics/src/main.ts
+++ 20-physics/src/main.ts
@@ -169,17 +169,18 @@ interface ObjectsToUpdate {
 
 const objectsToUpdate: ObjectsToUpdate[] = []
 
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5,
+})
+
 const createSphere = (radius, position) => {
   // Three.js mesh
-  const mesh = new THREE.Mesh(
-    new THREE.SphereGeometry(radius, 20, 20),
-    new THREE.MeshStandardMaterial({
-      metalness: 0.3,
-      roughness: 0.4,
-      envMap: environmentMapTexture,
-      envMapIntensity: 0.5,
-    })
-  )
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+  mesh.scale.set(radius, radius, radius)
   mesh.castShadow = true
   mesh.position.copy(position)
   scene.add(mesh)
