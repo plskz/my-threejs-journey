e419f5c27c07b5078e34c9f69abe6aa292cd6c89
Zai Santillan <zaisantillan14@gmail.com>
2024-01-06 20:13:32 +0800

    feat(21): add animations

--- 21-imported-models/src/main.ts
+++ 21-imported-models/src/main.ts
@@ -28,8 +28,15 @@ dracoLoader.setDecoderPath('/draco/')
 const gltfLoader = new GLTFLoader()
 gltfLoader.setDRACOLoader(dracoLoader)
 
-gltfLoader.load('/models/Duck/glTF-Draco/Duck.gltf', (gltf) => {
-  console.log(gltf)
+let mixer: THREE.AnimationMixer = null!
+
+gltfLoader.load('/models/Fox/glTF/Fox.gltf', (gltf) => {
+  mixer = new THREE.AnimationMixer(gltf.scene)
+  const action = mixer.clipAction(gltf.animations[0])
+  console.log(action)
+  action.play()
+
+  gltf.scene.scale.setScalar(0.025)
   scene.add(gltf.scene)
 })
 
@@ -126,6 +133,11 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - previousTime
   previousTime = elapsedTime
+
+  // Update mixer
+  if (mixer) {
+    mixer.update(deltaTime)
+  }
 
   // Update controls
   controls.update()
