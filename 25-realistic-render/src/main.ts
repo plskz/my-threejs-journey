ec7fa14df4e1622d9a62c348ddac888689a5b10f
Zai Santillan <zaisantillan14@gmail.com>
2024-01-13 20:45:17 +0800

    feat(25): add tone mapping

--- 25-realistic-render/src/main.ts
+++ 25-realistic-render/src/main.ts
@@ -116,6 +116,19 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+// Tone mapping
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 3
+
+gui.add(renderer, 'toneMapping', {
+  No: THREE.NoToneMapping,
+  Linear: THREE.LinearToneMapping,
+  Reinhard: THREE.ReinhardToneMapping,
+  Cineon: THREE.CineonToneMapping,
+  ACESFilmic: THREE.ACESFilmicToneMapping,
+})
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
+
 /**
  * Animate
  */
