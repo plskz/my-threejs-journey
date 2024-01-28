01510d2ed653c376fde45c52bd3fb9de421f75e3
Zai Santillan <zaisantillan14@gmail.com>
2024-01-28 21:25:53 +0800

    feat(42): add starter

---
+++ 42-first-r3f-application/src/components/Scene.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
+
+export default function Scene() {
+  return (
+    <Canvas>
+      <Experience />
+    </Canvas>
+  );
+}
