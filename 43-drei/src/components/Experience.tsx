85e9f0b9642a9423080da2def2f1e0bef4cae92f
Zai Santillan <zaisantillan14@gmail.com>
2024-02-03 11:10:11 +0800

    feat(43): add TransformControls

--- 43-drei/src/components/Experience.tsx
+++ 43-drei/src/components/Experience.tsx
@@ -1,9 +1,12 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, TransformControls } from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Experience() {
+  const cube = useRef<THREE.Mesh>(null!);
+
   return (
     <>
-      <OrbitControls />
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
@@ -13,10 +16,11 @@ export default function Experience() {
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
+      <TransformControls object={cube} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
