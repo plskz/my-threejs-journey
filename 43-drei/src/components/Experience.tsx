2bc7b85fc33c39f6bfa3cd9e014b9686b420bec9
Zai Santillan <zaisantillan14@gmail.com>
2024-02-03 13:04:28 +0800

    feat(43): finish

--- 43-drei/src/components/Experience.tsx
+++ 43-drei/src/components/Experience.tsx
@@ -1,7 +1,10 @@
 import {
+  Float,
   Html,
+  MeshReflectorMaterial,
   OrbitControls,
   PivotControls,
+  Text,
   TransformControls,
 } from "@react-three/drei";
 import { useRef } from "react";
@@ -40,16 +43,37 @@ export default function Experience() {
         </mesh>
       </PivotControls>
 
+      <Float speed={4} floatIntensity={4}>
+        <mesh position={[0, 2, 0]}>
+          <boxGeometry />
+          <meshStandardMaterial color="lightblue" />
+        </mesh>
+      </Float>
+
       <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
+        <Html
+          position={[0, 1, 0]}
+          center
+          distanceFactor={6}
+          occlude={[sphere, cube]}
+        >
+          <div className="select-none overflow-hidden whitespace-nowrap rounded-3xl bg-black/60 p-4 text-white">
+            <p>Cube</p>
+          </div>
+        </Html>
       </mesh>
       <TransformControls object={cube} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        {/* <meshStandardMaterial color="greenyellow" /> */}
+        <MeshReflectorMaterial resolution={1024} mirror={0.75} />
       </mesh>
+
+      {/* not working */}
+      {/* <Text>I LOVE R3F</Text> */}
     </>
   );
 }
