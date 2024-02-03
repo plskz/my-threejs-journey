52674d390bb2910fcff5aec8f05f88187a7b739d
Zai Santillan <zaisantillan14@gmail.com>
2024-02-03 11:46:59 +0800

    feat(43): add Html

--- 43-drei/src/components/Experience.tsx
+++ 43-drei/src/components/Experience.tsx
@@ -1,4 +1,5 @@
 import {
+  Html,
   OrbitControls,
   PivotControls,
   TransformControls,
@@ -7,6 +8,7 @@ import { useRef } from "react";
 
 export default function Experience() {
   const cube = useRef<THREE.Mesh>(null!);
+  const sphere = useRef<THREE.Mesh>(null!);
 
   return (
     <>
@@ -22,9 +24,19 @@ export default function Experience() {
         scale={100}
         fixed={true}
       >
-        <mesh position-x={-2}>
+        <mesh ref={sphere} position-x={-2}>
           <sphereGeometry />
           <meshStandardMaterial color="orange" />
+          <Html
+            position={[1, 1, 0]}
+            center
+            distanceFactor={6}
+            occlude={[sphere, cube]}
+          >
+            <div className="select-none overflow-hidden whitespace-nowrap rounded-3xl bg-black/60 p-4 text-white">
+              <p>Sphere</p>
+            </div>
+          </Html>
         </mesh>
       </PivotControls>
 
