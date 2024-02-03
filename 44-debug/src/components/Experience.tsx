f5023a9f1c6dab6ed4c4e9de0e1e57ca49cc32da
Zai Santillan <zaisantillan14@gmail.com>
2024-02-03 20:40:40 +0800

    feat(44): finish

--- 44-debug/src/components/Experience.tsx
+++ 44-debug/src/components/Experience.tsx
@@ -1,21 +1,55 @@
 import { OrbitControls } from "@react-three/drei";
+import { button, useControls } from "leva";
+import { Perf } from "r3f-perf";
 
 export default function Experience() {
+  const sphereConfig = useControls("Sphere", {
+    position: {
+      value: { x: -2, y: 0 },
+      step: 0.01,
+      joystick: "invertY",
+    },
+    color: "#ff0000",
+    visible: true,
+    clickMe: button(() => console.log("clicked!")),
+    choices: { options: ["a", "b", "c"] },
+  });
+
+  const boxConfig = useControls("Box", {
+    position: {
+      value: { x: 2, y: 0 },
+      step: 0.01,
+      joystick: "invertY",
+    },
+    color: "#00ff00",
+    visible: true,
+  });
+
   return (
     <>
+      {/* doesn't work */}
+      {/* <Perf position="top-left" /> */}
+
       <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <mesh position-x={-2}>
+      <mesh
+        position={[sphereConfig.position.x, sphereConfig.position.y, 0]}
+        visible={sphereConfig.visible}
+      >
         <sphereGeometry />
-        <meshStandardMaterial color="orange" />
+        <meshStandardMaterial color={sphereConfig.color} />
       </mesh>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh
+        position={[boxConfig.position.x, boxConfig.position.y, 0]}
+        scale={1.5}
+        visible={boxConfig.visible}
+      >
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
+        <meshStandardMaterial color={boxConfig.color} />
       </mesh>
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
--- 44-debug/src/components/Scene.tsx
+++ 44-debug/src/components/Scene.tsx
@@ -2,18 +2,22 @@
 
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import { Leva } from "leva";
 
 export default function Scene() {
   return (
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-      }}
-    >
-      <Experience />
-    </Canvas>
+    <>
+      <Leva collapsed />
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [-4, 3, 6],
+        }}
+      >
+        <Experience />
+      </Canvas>
+    </>
   );
 }
