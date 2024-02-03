c6423c155efc976b56ecc2a5d0bd3f2eb582c6ec
Zai Santillan <zaisantillan14@gmail.com>
2024-02-03 19:28:13 +0800

    feat(44): add starter

---
+++ 44-debug/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Scene from "@/components/Scene";
+
+export default function Home() {
+  return (
+    <main className="fixed left-0 top-0 h-full w-full overflow-hidden">
+      <Scene />
+    </main>
+  );
+}
