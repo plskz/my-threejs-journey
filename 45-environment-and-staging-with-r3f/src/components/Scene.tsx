d1ba53a47925ff0bf1a6b806fb0333d6e5576933
Zai Santillan <zaisantillan14@gmail.com>
2024-02-06 04:29:48 +0800

    feat(45): add starter

---
+++ 45-environment-and-staging-with-r3f/src/components/Scene.tsx
@@ -0,0 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
+
+export default function Scene() {
+  return (
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-8, 6, 12],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  );
+}
