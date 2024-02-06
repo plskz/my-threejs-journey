0c8ef8b0cb0bdb4ef81a672331d2db862fdfb738
Zai Santillan <zaisantillan14@gmail.com>
2024-02-06 08:10:39 +0800

    feat(45): add lights

--- 45-environment-and-staging-with-r3f/src/components/Experience.tsx
+++ 45-environment-and-staging-with-r3f/src/components/Experience.tsx
@@ -1,9 +1,13 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useHelper } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
 import { useRef } from "react";
+import * as THREE from "three";
 
 export default function Experience() {
+  const directionalLight = useRef<THREE.DirectionalLight>(null!);
+  useHelper(directionalLight, THREE.DirectionalLightHelper, 2);
+
   const cube = useRef<THREE.Mesh>(null!);
 
   useFrame((state, delta) => {
@@ -12,11 +16,17 @@ export default function Experience() {
 
   return (
     <>
+      <color attach="background" args={["skyblue"]} />
+
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
-      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <directionalLight
+        ref={directionalLight}
+        position={[1, 2, 3]}
+        intensity={4.5}
+      />
       <ambientLight intensity={1.5} />
 
       <mesh position-x={-2}>
