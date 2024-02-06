eef819b83ecfbc0669d9fd3dff7c0bf7d7e7b8ef
Zai Santillan <zaisantillan14@gmail.com>
2024-02-06 23:44:40 +0800

    feat(46): preload

--- 46-load-models-with-r3f/src/components/Model.tsx
+++ 46-load-models-with-r3f/src/components/Model.tsx
@@ -1,7 +1,9 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function Model() {
-  const model = useGLTF("./hamburger.glb");
+  const model = useGLTF("./hamburger-draco.glb");
 
   return <primitive object={model.scene} scale={0.35} />;
 }
+
+useGLTF.preload("./hamburger-draco.glb");
