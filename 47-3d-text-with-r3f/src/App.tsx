a62fbef966debe929eddefdaf0be09cd6f09ddeb
Zai Santillan <zaisantillan14@gmail.com>
2024-02-07 07:42:07 +0800

    feat(47): add starter

---
+++ 47-3d-text-with-r3f/src/App.tsx
@@ -0,0 +1,11 @@
+import Scene from "./components/Scene";
+
+function App() {
+  return (
+    <main className="fixed left-0 top-0 h-full w-full overflow-hidden">
+      <Scene />
+    </main>
+  );
+}
+
+export default App;
