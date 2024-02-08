ed1b3f274ee97fe955e7e0b4c769deb94a2f8960
Zai Santillan <zaisantillan14@gmail.com>
2024-02-08 23:50:05 +0800

    feat(49): occluding

--- 49-mouse-events-with-r3f/src/components/Experience.tsx
+++ 49-mouse-events-with-r3f/src/components/Experience.tsx
@@ -25,7 +25,7 @@ export default function Experience() {
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <mesh position-x={-2}>
+      <mesh position-x={-2} onClick={(e) => e.stopPropagation()}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
