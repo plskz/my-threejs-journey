1b286b9ef11dfa4197432a24e2dc525c486cd179
Zai Santillan <zaisantillan14@gmail.com>
2024-02-10 12:11:02 +0800

    feat(51): add environment

--- 51-fun-and-simple-portfolio-with-r3f/src/components/Experience.tsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/components/Experience.tsx
@@ -1,10 +1,11 @@
-import { Center, OrbitControls } from "@react-three/drei";
+import { Center, Environment, OrbitControls } from "@react-three/drei";
 import { Model } from "./Model";
 
 export default function Experience() {
   return (
     <>
       <color args={["#241a1a"]} attach="background" />
+      <Environment preset="city" />
 
       <OrbitControls makeDefault />
 
