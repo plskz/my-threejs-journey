1ac3fcd184abcaba1c6f5dba1379c9e0d8be204a
Zai Santillan <zaisantillan14@gmail.com>
2024-02-10 12:53:30 +0800

    feat(51): add iframe

--- 51-fun-and-simple-portfolio-with-r3f/src/components/Model.tsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/components/Model.tsx
@@ -1,7 +1,23 @@
-import { useGLTF } from "@react-three/drei";
+import { Html, useGLTF } from "@react-three/drei";
 
 export function Model() {
-  const computer = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf",);
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf",
+  );
 
-  return <primitive object={computer.scene} position-y={-1.2} />;
+  return (
+    <primitive object={computer.scene} position-y={-1.2}>
+      <Html
+        transform
+        distanceFactor={1.17}
+        position={[0, 1.56, -1.4]}
+        rotation-x={-0.256}
+      >
+        <iframe
+          className="h-[670px] w-[1024px] rounded-3xl border-none bg-black"
+          src="https://plskz-me.vercel.app/"
+        />
+      </Html>
+    </primitive>
+  );
 }
