e36bc97f4500d88b876f13340548da3b5d622beb
Zai Santillan <zaisantillan14@gmail.com>
2024-02-10 12:08:46 +0800

    feat(51): add laptop

--- 51-fun-and-simple-portfolio-with-r3f/src/components/Experience.tsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/components/Experience.tsx
@@ -1,14 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
+import { Center, OrbitControls } from "@react-three/drei";
+import { Model } from "./Model";
 
 export default function Experience() {
   return (
     <>
+      <color args={["#241a1a"]} attach="background" />
+
       <OrbitControls makeDefault />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Center>
+        <Model />
+      </Center>
     </>
   );
 }
---
+++ 51-fun-and-simple-portfolio-with-r3f/src/components/Model.tsx
@@ -0,0 +1,7 @@
+import { useGLTF } from "@react-three/drei";
+
+export function Model() {
+  const computer = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf",);
+
+  return <primitive object={computer.scene} />;
+}
