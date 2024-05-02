7c74325f3c11463ad450026e62a3ff73556eea3e
Zai Santillan <zaisantillan14@gmail.com>
2024-05-03 04:21:16 +0800

    feat(32): add starter

---
+++ 32-coffee-smoke-shader/src/main.ts
@@ -0,0 +1,111 @@
+import * as THREE from 'three'
+import './style.css'
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/Addons.js'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')!
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const textureLoader = new THREE.TextureLoader()
+const gltfLoader = new GLTFLoader()
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  25,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.x = 8
+camera.position.y = 10
+camera.position.z = 12
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.y = 3
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Model
+ */
+gltfLoader.load('./bakedModel.glb', (gltf) => {
+  // gltf.scene.getObjectByName('baked').material.map.anisotropy = 8 // warning: object is possibly 'undefined'
+  const bakedObject = gltf.scene.getObjectByName('baked')
+  if (bakedObject instanceof THREE.Mesh) {
+    const material = bakedObject.material as THREE.MeshStandardMaterial
+    if (material.map) {
+      material.map.anisotropy = 8
+    }
+  }
+
+  scene.add(gltf.scene)
+})
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
