80c529584ea6fcaded2c607dd69ec358fcc5ccf2
Zai Santillan <zaisantillan14@gmail.com>
2024-05-08 09:26:29 +0800

    feat(35): ambient light

--- 35-lights-shading-shaders/src/shaders/shading/fragment.glsl
+++ 35-lights-shading-shaders/src/shaders/shading/fragment.glsl
@@ -1,8 +1,18 @@
 uniform vec3 uColor;
 
+#include ../utils/ambientLight.glsl
+
 void main()
 {
     vec3 color = uColor;
+
+    // Lights
+    vec3 light = vec3(0.0);
+     light += ambientLight(
+        vec3(1.0), // Light color
+        0.03       // Light intensity
+    );
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
