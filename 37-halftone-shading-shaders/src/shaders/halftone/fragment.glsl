c5e988266b9f38143076ac85e07df59e130c1466
Zai Santillan <zaisantillan14@gmail.com>
2024-05-09 10:09:55 +0800

    feat(37): radius and intensity

--- 37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
+++ 37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
@@ -33,14 +33,20 @@ void main()
     color *= light;
 
     // Halftone
-    float repetitions = 10.0;
+    float repetitions = 50.0;
+    vec3 direction = vec3(0.0, -1.0, 0.0);
+    float low = - 0.8;
+    float high = 1.5;
+
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low, high, intensity);
 
     vec2 uv = gl_FragCoord.xy / uResolution.y;
     uv *= repetitions;
     uv = mod(uv, 1.0);
 
     float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5, point);
+    point = 1.0 - step(0.5 * intensity, point);
 
     // Final color
     gl_FragColor = vec4(point, point, point, 1.0);
