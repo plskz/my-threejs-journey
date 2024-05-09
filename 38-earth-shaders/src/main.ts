c9068550493645058b59bea3ea675813c1e9c611
Zai Santillan <zaisantillan14@gmail.com>
2024-05-09 16:28:50 +0800

    feat(38): sun

--- 38-earth-shaders/src/main.ts
+++ 38-earth-shaders/src/main.ts
@@ -33,7 +33,9 @@ earthDayTexture.colorSpace = THREE.SRGBColorSpace
 const earthNightTexture = textureLoader.load('./earth/night.jpg')
 earthNightTexture.colorSpace = THREE.SRGBColorSpace
 
-const earthSpecularCloudsTexture = textureLoader.load('./earth/specularClouds.jpg')
+const earthSpecularCloudsTexture = textureLoader.load(
+  './earth/specularClouds.jpg'
+)
 
 // Mesh
 const earthGeometry = new THREE.SphereGeometry(2, 64, 64)
@@ -44,10 +46,43 @@ const earthMaterial = new THREE.ShaderMaterial({
     uDayTexture: new THREE.Uniform(earthDayTexture),
     uNightTexture: new THREE.Uniform(earthNightTexture),
     uSpecularCloudsTexture: new THREE.Uniform(earthSpecularCloudsTexture),
+    uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
   },
 })
 const earth = new THREE.Mesh(earthGeometry, earthMaterial)
 scene.add(earth)
+
+/**
+ * Sun
+ */
+// Coordinates
+const sunSpherical = new THREE.Spherical(1, Math.PI * 0.5, 0.5)
+const sunDirection = new THREE.Vector3()
+
+// Update
+const updateSun = () => {
+  // Sun direction
+  sunDirection.setFromSpherical(sunSpherical)
+
+  // Debug
+  debugSun.position.copy(sunDirection).multiplyScalar(5)
+
+  // Uniforms
+  earthMaterial.uniforms.uSunDirection.value.copy(sunDirection)
+}
+
+// Debug
+const debugSun = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+scene.add(debugSun)
+
+// Tweaks
+gui.add(sunSpherical, 'phi').min(0).max(Math.PI).onChange(updateSun)
+gui.add(sunSpherical, 'theta').min(-Math.PI).max(Math.PI).onChange(updateSun)
+
+updateSun()
 
 /**
  * Sizes
