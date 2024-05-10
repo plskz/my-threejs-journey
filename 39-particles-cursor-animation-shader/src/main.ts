fda463c68fd8ab8e5d098e769103d975b8e0aeec
Zai Santillan <57343545+plskz@users.noreply.github.com>
2024-05-10 13:09:13 +0800

    feat(39): particles cursor animation (#6)
    * feat(39): particles
    
    * feat(39): 2d canvas
    
    * feat(39): draw cursor on coordinates
    
    * feat(39): fade out
    
    * feat(39): displacement as a texture
    
    * feat(39): displacement animation
    
    * feat(39): fix the canvas bug
    
    * feat(39): trail
    
    * fix(39): displacement intensity
    
    i forgot to add this
    
    * feat(39): speed alpha

--- 39-particles-cursor-animation-shader/src/main.ts
+++ 39-particles-cursor-animation-shader/src/main.ts
@@ -77,9 +77,103 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
 
 /**
+ * Displacement
+ */
+const displacement = {
+  canvas: document.createElement('canvas'),
+  context: null as CanvasRenderingContext2D | null,
+  glowImage: new Image(),
+  interactivePlane: new THREE.Mesh(),
+  raycaster: new THREE.Raycaster(),
+  screenCursor: new THREE.Vector2(),
+  canvasCursor: new THREE.Vector2(),
+  canvasCursorPrevious: new THREE.Vector2(),
+  texture: new THREE.CanvasTexture(null!),
+}
+
+// 2D canvas
+displacement.canvas.style.position = 'fixed'
+displacement.canvas.width = 128
+displacement.canvas.height = 128
+displacement.canvas.style.width = '256px'
+displacement.canvas.style.height = '256px'
+displacement.canvas.style.top = '0'
+displacement.canvas.style.left = '0'
+displacement.canvas.style.zIndex = '10'
+document.body.append(displacement.canvas)
+
+// Context
+displacement.context = displacement.canvas.getContext('2d')
+displacement.context!.fillRect(
+  0,
+  0,
+  displacement.canvas.width,
+  displacement.canvas.height
+)
+
+// Glow image
+displacement.glowImage.src = './glow.png'
+
+// Interactive plane
+displacement.interactivePlane = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshBasicMaterial({ color: 'red', side: THREE.DoubleSide })
+)
+displacement.interactivePlane.visible = false
+scene.add(displacement.interactivePlane)
+
+// Raycaster
+displacement.raycaster = new THREE.Raycaster()
+
+// Coordinates
+displacement.screenCursor = new THREE.Vector2(9999, 9999)
+displacement.canvasCursor = new THREE.Vector2(9999, 9999)
+displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999)
+
+window.addEventListener('pointermove', (event) => {
+  displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1
+  displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1
+})
+
+// Texture
+displacement.texture = new THREE.CanvasTexture(displacement.canvas)
+
+/**
  * Particles
  */
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 32, 32)
+const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128)
+particlesGeometry.setIndex(null)
+particlesGeometry.deleteAttribute('normal')
+
+const intensitiesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+)
+const anglesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+)
+
+for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
+  intensitiesArray[i] = Math.random()
+  anglesArray[i] = Math.random() * Math.PI * 2
+}
+
+particlesGeometry.setAttribute(
+  'aIntensity',
+  new THREE.BufferAttribute(intensitiesArray, 1)
+)
+particlesGeometry.setAttribute(
+  'aAngle',
+  new THREE.BufferAttribute(anglesArray, 1)
+)
+
+for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
+  intensitiesArray[i] = Math.random()
+}
+
+particlesGeometry.setAttribute(
+  'aIntensity',
+  new THREE.BufferAttribute(intensitiesArray, 1)
+)
 
 const particlesMaterial = new THREE.ShaderMaterial({
   vertexShader: particlesVertexShader,
@@ -91,7 +185,10 @@ const particlesMaterial = new THREE.ShaderMaterial({
         sizes.height * sizes.pixelRatio
       )
     ),
+    uPictureTexture: new THREE.Uniform(textureLoader.load('./picture-1.png')),
+    uDisplacementTexture: new THREE.Uniform(displacement.texture),
   },
+  blending: THREE.AdditiveBlending,
 })
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
@@ -102,6 +199,57 @@ scene.add(particles)
 const tick = () => {
   // Update controls
   controls.update()
+
+  /**
+   * Raycaster
+   */
+  displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
+  const intersections = displacement.raycaster.intersectObject(
+    displacement.interactivePlane
+  )
+
+  if (intersections.length) {
+    const uv = intersections[0].uv!
+
+    displacement.canvasCursor.x = uv.x * displacement.canvas.width
+    displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height
+  }
+  displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
+
+  /**
+   * Displacement
+   */
+  // Fade out
+  displacement.context!.globalCompositeOperation = 'source-over'
+  displacement.context!.globalAlpha = 0.02
+  displacement.context!.fillRect(
+    0,
+    0,
+    displacement.canvas.width,
+    displacement.canvas.height
+  )
+
+  // Speed alpha
+  const cursorDistance = displacement.canvasCursorPrevious.distanceTo(
+    displacement.canvasCursor
+  )
+  displacement.canvasCursorPrevious.copy(displacement.canvasCursor)
+  const alpha = Math.min(cursorDistance * 0.1, 1)
+
+  // Draw glow
+  const glowSize = displacement.canvas.width * 0.25
+  displacement.context!.globalCompositeOperation = 'lighten'
+  displacement.context!.globalAlpha = alpha
+  displacement.context!.drawImage(
+    displacement.glowImage,
+    displacement.canvasCursor.x - glowSize * 0.5,
+    displacement.canvasCursor.y - glowSize * 0.5,
+    glowSize,
+    glowSize
+  )
+
+  // Texture
+  displacement.texture.needsUpdate = true
 
   // Render
   renderer.render(scene, camera)
