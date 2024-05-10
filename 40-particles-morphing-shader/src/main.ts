56c7ae45be85ffb1ed8ccaf544d6489a837c9622
Zai Santillan <zaisantillan14@gmail.com>
2024-05-10 18:14:32 +0800

    feat(40): load the models

--- 40-particles-morphing-shader/src/main.ts
+++ 40-particles-morphing-shader/src/main.ts
@@ -45,10 +45,12 @@ window.addEventListener('resize', () => {
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
   // Materials
-  particles.material.uniforms.uResolution.value.set(
-    sizes.width * sizes.pixelRatio,
-    sizes.height * sizes.pixelRatio
-  )
+  if (particles) {
+    particles.material.uniforms.uResolution.value.set(
+      sizes.width * sizes.pixelRatio,
+      sizes.height * sizes.pixelRatio
+    )
+  }
 
   // Update camera
   camera.aspect = sizes.width / sizes.height
@@ -96,33 +98,36 @@ renderer.setClearColor(debugObject.clearColor)
 /**
  * Particles
  */
-const particles: any = {}
+let particles: any = null
 
-// Geometry
-particles.geometry = new THREE.SphereGeometry(3)
-particles.geometry.setIndex(null)
+gltfLoader.load('./models.glb', (gltf) => {
+  particles = {}
 
-// Material
-particles.material = new THREE.ShaderMaterial({
-  vertexShader: particlesVertexShader,
-  fragmentShader: particlesFragmentShader,
-  uniforms: {
-    uSize: new THREE.Uniform(0.4),
-    uResolution: new THREE.Uniform(
-      new THREE.Vector2(
-        sizes.width * sizes.pixelRatio,
-        sizes.height * sizes.pixelRatio
-      )
-    ),
+  // Geometry
+  particles.geometry = new THREE.SphereGeometry(3)
+  particles.geometry.setIndex(null)
 
-  },
-  blending: THREE.AdditiveBlending,
-  depthWrite: false,
+  // Materials
+  particles.material = new THREE.ShaderMaterial({
+    vertexShader: particlesVertexShader,
+    fragmentShader: particlesFragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(0.4),
+      uResolution: new THREE.Uniform(
+        new THREE.Vector2(
+          sizes.width * sizes.pixelRatio,
+          sizes.height * sizes.pixelRatio
+        )
+      ),
+    },
+    blending: THREE.AdditiveBlending,
+    depthWrite: false,
+  })
+
+  // Points
+  particles.points = new THREE.Points(particles.geometry, particles.material)
+  scene.add(particles.points)
 })
-
-// Points
-particles.points = new THREE.Points(particles.geometry, particles.material)
-scene.add(particles.points)
 
 /**
  * Animate
