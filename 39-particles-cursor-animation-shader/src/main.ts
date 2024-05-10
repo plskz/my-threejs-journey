0650e8808dbe9cbfd558fdd8121b58f6b7c731b8
Zai Santillan <zaisantillan14@gmail.com>
2024-05-10 10:04:38 +0800

    feat(39): draw cursor on coordinates

--- 39-particles-cursor-animation-shader/src/main.ts
+++ 39-particles-cursor-animation-shader/src/main.ts
@@ -83,12 +83,18 @@ const displacement = {
   canvas: document.createElement('canvas'),
   context: null as CanvasRenderingContext2D | null,
   glowImage: new Image(),
+  interactivePlane: new THREE.Mesh(),
+  raycaster: new THREE.Raycaster(),
+  screenCursor: new THREE.Vector2(),
+  canvasCursor: new THREE.Vector2(),
 }
 
 // 2D canvas
 displacement.canvas.style.position = 'fixed'
-displacement.canvas.style.width = '512px'
-displacement.canvas.style.height = '512px'
+displacement.canvas.width = 128
+displacement.canvas.height = 128
+displacement.canvas.style.width = '256px'
+displacement.canvas.style.height = '256px'
 displacement.canvas.style.top = '0'
 displacement.canvas.style.left = '0'
 displacement.canvas.style.zIndex = '10'
@@ -105,6 +111,25 @@ displacement.context!.fillRect(
 
 // Glow image
 displacement.glowImage.src = './glow.png'
+
+// Interactive plane
+displacement.interactivePlane = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshBasicMaterial({ color: 'red' })
+)
+scene.add(displacement.interactivePlane)
+
+// Raycaster
+displacement.raycaster = new THREE.Raycaster()
+
+// Coordinates
+displacement.screenCursor = new THREE.Vector2(9999, 9999)
+displacement.canvasCursor = new THREE.Vector2(9999, 9999)
+
+window.addEventListener('pointermove', (event) => {
+  displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1
+  displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1
+})
 
 /**
  * Particles
@@ -133,6 +158,36 @@ scene.add(particles)
 const tick = () => {
   // Update controls
   controls.update()
+
+  /**
+   * Raycaster
+   */
+  displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
+  const intersections = displacement.raycaster.intersectObject(
+    displacement.interactivePlane
+  )
+
+  if (intersections.length) {
+    const uv = intersections[0].uv!
+
+    displacement.canvasCursor.x = uv.x * displacement.canvas.width
+    displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas.height
+  }
+  displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
+
+  /**
+   * Displacement
+   */
+  // Draw glow
+  const glowSize = displacement.canvas.width * 0.25
+  displacement.context!.globalCompositeOperation = 'lighten'
+  displacement.context!.drawImage(
+    displacement.glowImage,
+    displacement.canvasCursor.x - glowSize * 0.5,
+    displacement.canvasCursor.y - glowSize * 0.5,
+    glowSize,
+    glowSize
+  )
 
   // Render
   renderer.render(scene, camera)
