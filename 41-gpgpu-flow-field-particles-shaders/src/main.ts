e9026b65cfed44ae46baa3db32ac9ffa658897df
Zai Santillan <zaisantillan14@gmail.com>
2024-05-12 18:10:42 +0800

    style(41): format

--- 41-gpgpu-flow-field-particles-shaders/src/main.ts
+++ 41-gpgpu-flow-field-particles-shaders/src/main.ts
@@ -110,21 +110,29 @@ gpgpu.computation = new GPUComputationRenderer(gpgpu.size, gpgpu.size, renderer)
 // Base particles
 const baseParticlesTexture = gpgpu.computation.createTexture()
 
-for(let i = 0; i < baseGeometry.count; i++)
-  {
-      const i3 = i * 3
-      const i4 = i * 4
-  
-      // Position based on geometry
-      baseParticlesTexture.image.data[i4 + 0] = baseGeometry.instance.attributes.position.array[i3 + 0]
-      baseParticlesTexture.image.data[i4 + 1] = baseGeometry.instance.attributes.position.array[i3 + 1]
-      baseParticlesTexture.image.data[i4 + 2] = baseGeometry.instance.attributes.position.array[i3 + 2]
-      baseParticlesTexture.image.data[i4 + 3] = 0
-  }
+for (let i = 0; i < baseGeometry.count; i++) {
+  const i3 = i * 3
+  const i4 = i * 4
+
+  // Position based on geometry
+  baseParticlesTexture.image.data[i4 + 0] =
+    baseGeometry.instance.attributes.position.array[i3 + 0]
+  baseParticlesTexture.image.data[i4 + 1] =
+    baseGeometry.instance.attributes.position.array[i3 + 1]
+  baseParticlesTexture.image.data[i4 + 2] =
+    baseGeometry.instance.attributes.position.array[i3 + 2]
+  baseParticlesTexture.image.data[i4 + 3] = 0
+}
 
 // Particles variable
-gpgpu.particlesVariable = gpgpu.computation.addVariable('uParticles', gpgpuParticlesShader, baseParticlesTexture)
-gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [ gpgpu.particlesVariable ])
+gpgpu.particlesVariable = gpgpu.computation.addVariable(
+  'uParticles',
+  gpgpuParticlesShader,
+  baseParticlesTexture
+)
+gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [
+  gpgpu.particlesVariable,
+])
 
 // Init
 gpgpu.computation.init()
@@ -132,7 +140,10 @@ gpgpu.computation.init()
 // Debug
 gpgpu.debug = new THREE.Mesh(
   new THREE.PlaneGeometry(3, 3),
-  new THREE.MeshBasicMaterial({ map: gpgpu.computation.getCurrentRenderTarget(gpgpu.particlesVariable).texture })
+  new THREE.MeshBasicMaterial({
+    map: gpgpu.computation.getCurrentRenderTarget(gpgpu.particlesVariable)
+      .texture,
+  })
 )
 gpgpu.debug.position.x = 3
 scene.add(gpgpu.debug)
