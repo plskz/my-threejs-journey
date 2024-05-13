e66c1a654bd204ab656b7c79c4cd5c2fb9cc59ab
Zai Santillan <zaisantillan14@gmail.com>
2024-05-13 12:49:50 +0800

    feat(42): basic wobble

--- 42-wobbly-sphere-shader/src/shaders/wobble/vertex.glsl
+++ 42-wobbly-sphere-shader/src/shaders/wobble/vertex.glsl
@@ -1,4 +1,11 @@
+#include ../includes/simplexNoise4d.glsl
+
 void main()
 {
-
+    // Wobble
+    float wobble = simplexNoise4d(vec4(
+        csm_Position, // XYZ
+        0.0           // W
+    ));
+    csm_Position += wobble * normal;
 }
